3e4e21a93ca205d0b1bae87e7891994f5c45cb6e
= <=>
2025-03-04 14:34:07 +0100

    cambio menu

--- componentes/scripts/menuHamburguesa.js
+++ componentes/scripts/menuHamburguesa.js
@@ -1,5 +1,11 @@
-$(function () {
-    $('#menu-toggle').on('click', function () {
-        $('#nav-menu').toggleClass('active');
-    });
-});
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
